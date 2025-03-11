4ccb3ab5721b6364c06fb37af7a2839dbba1c557
Vckers <34670517+Vckers@users.noreply.github.com>
2025-03-11 11:47:18 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["v4.vckers.cn"];
+pub const RS_PUB_KEY: &str = "X0gnU1pZu73M1csy1AMqw2H6IJrP7m092JSaQ8zcZL8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
